34be0fda3e01c8fa5599d1dea92bbfcfa3e91d83
Jason Yeung <yeung.jsn@gmail.com>
2022-05-06 15:13:44 -0400

    Updated with preprocessing script for pulling data

--- data/preprocessing.R
+++ data/preprocessing.R
@@ -5,5 +5,19 @@ library(DelayedArray)
 
 data("se_soybean_cn_sub")
 
+# pull normalized counts matrix
+matrix <- as.matrix(seed(assay(se_soybean_cn_sub)))
+write.table(matrix, file='normalized_counts.csv', sep=',')
 
-matrix <- seed(assay(se_soybean_cn_sub))
+# generate sample info matrix
+array_id <- c("S1.1", "S1.2", "S1.3", "S2.1", "S2.2", "S2.3", "S3.1", "S3.2", "S3.3")
+stage <- c("early", "early", "early", "middle", "middle", "middle", "late", "late", "late")
+replicate_num <- c(1 ,2, 3, 1, 2, 3, 1, 2, 3)
+
+sample_info <- data.frame(array_id, stage, replicate_num)
+write.table(sample_info, file='sample_info.csv', sep=',')
+
+de_results <- rowData(se_soybean_cn_sub)
+
+write.table(de_results, file="./de_results.csv", sep=",", row.names = TRUE)
+
